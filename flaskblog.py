b62d8963dcbc7bba37670d28e99292b481dd7884
Emmzy17 <emmanueladjerry@gmail.com>
2022-02-08 14:19:11 -0800

    Blog

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Emjay',
+        'title':'Why the west rule for now',
+        'content':'Cos they kinda suck',
+        'date': '25th Jan, 2022'
+    },
+    {
+        'author':'Lesley',
+        'title':'Dave Expedition',
+        'content':'I don\'t know my self',
+        'date': '14th, Feb, 2022'
+    },
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About Page')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
