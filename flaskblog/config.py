dbf67e5ff12dbd8bbecb7d36d6cdd6db7d4a0984
Emmzy17 <emmanueladjerry@gmail.com>
2022-03-25 18:50:19 +0100

    tweaked for heroku deployment

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://wabmceiasacllf:5dd711cfa3fcdb8eba99b8c8bfdfa4801e2ed62895ae84d23add1bdb5e27552e@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d4nb5qvpbgmuh3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.gmail.com'
